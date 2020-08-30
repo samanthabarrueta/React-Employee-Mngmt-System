d01d492aa86317d91bfe1643c53ef26b746403f6
Samantha <samanthabarrueta@gmail.com>
2020-08-30 17:04:16 -0500

    restructure files/folders

--- ems/src/App.js
+++ ems/src/App.js
@@ -1,26 +1,8 @@
 import React from 'react';
-import Wrapper from "./components/Wrapper";
-import EmployeeCard from "./components/EmployeeCard";
-import employees from "./employees.json"
+import EMSContainer from "./components/EMSContainer"
 
-class App extends React.Component {
-
-  state = {
-    employees
-  };
-
-  render() {
-    return (
-      this.state.employees.map(employee => (<Wrapper>
-      <EmployeeCard
-      name = {employee.firstName + " " + employee.lastName}
-      role = {employee.role}
-      phoneNumber = {employee.phoneNumber}
-      emailAddress= {employee.emailAddress}
-      /></Wrapper>
-      ))   
-    );
-  };  
+function App() {
+  return <EMSContainer />; 
 };
 
 export default App;
