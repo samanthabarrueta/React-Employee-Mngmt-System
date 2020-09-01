d32416e72c58d3fb2844e5922245aee330fe645b
Samantha <samanthabarrueta@gmail.com>
2020-08-31 22:30:27 -0500

    search form component

---
+++ ems/src/components/SearchForm/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function SearchForm(props) {
+  return (
+    <form>
+      <div className="form-group">
+        <input
+          onChange={props.handleInputChange}
+          value={props.value}
+          name="search"
+          type="text"
+          className="form-control"
+          placeholder="Search For an Employee"
+          id="search"
+        />
+        <br />
+        <button onClick={props.handleFormSubmit} className="btn btn-primary">
+          Search
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default SearchForm;
