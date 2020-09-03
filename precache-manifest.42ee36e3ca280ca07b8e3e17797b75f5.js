7cfd57a9926b2c7d37c2bad6bdd0da3e5ade4c3e
Samantha <samanthabarrueta@gmail.com>
2020-09-02 19:07:39 -0500

    Updates

---
+++ precache-manifest.42ee36e3ca280ca07b8e3e17797b75f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4872e6469d2e2d98e668390827d1d391",
+    "url": "/React-Employee-Mngmt-System/index.html"
+  },
+  {
+    "revision": "789642a51142fc29c780",
+    "url": "/React-Employee-Mngmt-System/static/css/main.5a28b347.chunk.css"
+  },
+  {
+    "revision": "a86419be144b29025fb9",
+    "url": "/React-Employee-Mngmt-System/static/js/2.3437110f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-Employee-Mngmt-System/static/js/2.3437110f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "789642a51142fc29c780",
+    "url": "/React-Employee-Mngmt-System/static/js/main.3ebaf435.chunk.js"
+  },
+  {
+    "revision": "04f923275eb5abc24782",
+    "url": "/React-Employee-Mngmt-System/static/js/runtime-main.d5e96bfe.js"
+  }
+]);
